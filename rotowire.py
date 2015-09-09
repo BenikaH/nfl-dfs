93c3ca09ccc0065493343aeab2be0b94b3379812
MurrDogg4 <rm643dplay@gmail.com>
2015-09-09 18:43:12 -0400

    Fixed rotowire problems and added new columns

--- rotowire.py
+++ rotowire.py
@@ -1,20 +1,25 @@
+#!/usr/local/bin/python2.7
+
 from bs4 import BeautifulSoup
 import requests
-import csv
-from datetime import date, datetime, timedelta
-import time
 import MySQLdb
 
-tdate = date.today()
-tdate = tdate.strftime("%m/%d/%Y")
+
+headerList = ['playerID', 'pos', 'FullName', 'Lastname', 'Firstname', 'Team', 'Opp', 'Spread', 'OverUnder', 'ML', 'DKSalary', \
+            'DKP', 'DKValue', 'FDSalary', 'FDP', 'FDValue', 'webLink']
+
+f = open('weekinfo.txt', 'r')
+ftext = f.read().split(',')
+weekNum = int(ftext[0])
+
+# weekNum = int(raw_input("Week number? "))
+
+            
 r = requests.get("http://www.rotowire.com/daily/nfl/optimizer.htm?site=DraftKings").text
 soup = BeautifulSoup(r)
 
-weekNum = int(raw_input("Week number? "))
 playerSet = soup.find_all("tr", {"class" : "playerSet"})
 
-headerList = ['playerID', 'FullName', 'Team', 'Opp', 'Pos', 'DKSalary', 'DKP', 'DKValue', 'FirstName', 'LastName',\
- 'FDSalary', 'FDP', 'FDValue', 'webLink']
 player = []
 playerList = []
 
@@ -27,7 +32,7 @@ for players in playerSet:
         plid = 99999
     player.append(plid)
     rows = players.find_all("td")
-    for row in rows[1:-1]:
+    for row in rows[1:11]:
         for tag in row.find_all('span'):        # Remove span tags that have prob/quest/etc. in them
             tag.replace_with('')
         item = row.text.strip()
@@ -37,20 +42,14 @@ for players in playerSet:
                     item = item.replace(char, '')
             item = int(item)
         player.append(item)
-    plname = player[1].split(', ')
-    if len(plname) > 1:
-        for i in plname:
-            player.append(i)
-    else:
-        for i in range(0,2):
-            player.append('')
     for i in range(0,3):        # Placeholder for FD results
         player.append(0)
     player.append(plLink)
     playerList.append(player)
     player = []
 
-print playerList
+# print playerList
+
 # Fanduel Projections
 r = requests.get("http://www.rotowire.com/daily/nfl/optimizer.htm?site=FanDuel").text
 soup = BeautifulSoup(r)
@@ -70,7 +69,7 @@ for players in playerSet:
         plid = 99999
     player.append(plid)
     rows = players.find_all("td")
-    for row in rows[1:-1]:
+    for row in rows[1:11]:
         for tag in row.find_all('span'):        # Remove span tags that have prob/quest/etc. in them
             tag.replace_with('')
         item = row.text.strip()
@@ -80,13 +79,6 @@ for players in playerSet:
                     item = item.replace(char, '')
             item = int(item)
         player.append(item)
-    plname = player[1].split(', ')
-    if len(plname) > 1:
-        for i in plname:
-            player.append(i)
-    else:
-        for i in range(0,2):
-            player.append('')
     player.append(plLink)
     fdplayerList.append(player)
     player = []
@@ -94,13 +86,21 @@ for players in playerSet:
 for dplayer in playerList:
     for fplayer in fdplayerList:
         if dplayer[0] == fplayer[0]:
-            dplayer[10] = fplayer[5]
-            dplayer[11] = float(fplayer[6])
-            dplayer[12] = fplayer[7]
+            dplayer[11] = fplayer[8]
+            dplayer[12] = float(fplayer[9])
+            dplayer[13] = fplayer[10]
             continue
-            
 
-print playerList[:1]
+### Add first and last name
+for player in playerList:
+    plname = player[2].split(', ')
+    if len(plname) == 2:
+        player.insert(3, plname[0])
+        player.insert(3, plname[1])
+    else:
+        player.insert(3, '')
+        player.insert(3, '')
+
 
 con = MySQLdb.connect('localhost', 'root', '', 'test')
 
@@ -111,11 +111,12 @@ x.execute(query)
 for row in playerList:
     print row
     with con:
-        query = "INSERT INTO rotowire_wkly_proj (week, player_id, playernm_full, team, opp, pos, dk_salary, \
-            dkp, dk_value, playernm_first, playernm_last, fd_salary, fdp, fd_value, weblink) \
-                VALUES (%d, %d, "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", %d, %1.2f, %1.2f, "'"%s"'", "'"%s"'", \
-                %d, %1.2f, %1.2f, "'"%s"'")" % \
-                (weekNum, int(row[0]), row[1], row[2], row[3], row[4], row[5], \
-            round(float(row[6]),2), round(float(row[7]),2), row[8], row[9], row[10], round(float(row[11]),2), round(float(row[12]),2), row[13])
+        query = "INSERT INTO rotowire_wkly_proj (week, player_id, pos, playernm_full, playernm_last, playernm_first, \
+            team, opp, spread, over_under, ml, dk_salary, dkp, dk_value, fd_salary, fdp, fd_value, weblink) \
+                VALUES (%d, %d, "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", \
+                "'"%s"'", "'"%s"'", "'"%s"'", %d, %1.2f, %1.2f, %d, %1.2f, %1.2f, "'"%s"'")" % \
+                (weekNum, int(row[0]), row[1], row[2], row[3], row[4], row[5], row[6], \
+                row[7], row[8], row[9], int(row[10]), round(float(row[11]),2), round(float(row[12]),2), \
+                int(row[13]), round(float(row[14]),2), round(float(row[15]),2), row[16])
         x = con.cursor()
         x.execute(query)
